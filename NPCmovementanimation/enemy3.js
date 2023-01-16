f0389ca6a415e15f597c188462021b203f0ba519
unknown <gbemilesanmi@gmail.com>
2023-01-16 14:53:09 +0100

    NPC enemy 3 movement

--- NPCmovementanimation/enemy3.js
+++ NPCmovementanimation/enemy3.js
@@ -1,12 +1,3 @@
-/**@type {HTMLCanvasElement}*/ 
-const canvas = document.getElementById("canvas1")
-const ctx = canvas.getContext("2d")
-const CANVAS_WIDTH = canvas.width = 500
-const CANVAS_HEIGHT = canvas.height = 1000
-const numberofenemies = 50
-const enemiesArray = []
-let gameFrame = 0
-
 class Enemy{
     constructor(){
         this.image = new Image()
@@ -18,7 +9,9 @@ class Enemy{
         this.width = this.spriteWidth / 2
         this.x = Math.random() * (CANVAS_WIDTH - this.width)
         this.y = Math.random() * (CANVAS_HEIGHT - this.height)
-
+        this.angle = 0
+        this.curve = Math.random() * 400 + 50
+        this.angleSpeed = Math.random() * 2 + 0.5
         //controls the speed of the enemies 
         this.frame = 0
 
@@ -26,10 +19,11 @@ class Enemy{
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
     }
     update(){
-        //this.x = 0
-        //this.y = 0 
+        this.x = canvas.width/2 * Math.cos(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2)
+        this.y = canvas.height/2 * Math.sin(this.angle * Math.PI/270) + (canvas.height/2 - this.height/2)
         if (this.x + this.width < 0) {
             this.x = canvas.width}       
+        this.angle += this.angleSpeed
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame ++ }
     }
@@ -37,18 +31,3 @@ class Enemy{
         ctx.drawImage(this.image , this.frame * this.spriteWidth, 0, this.spriteWidth,this.spriteheight, this.x , this.y , this.width , this.height)
     }
 }
-
-for (let i = 0;i < numberofenemies;i++){
-    enemiesArray.push(new Enemy())
-}
-
-function animate() {
-    ctx.clearRect(0 , 0,CANVAS_WIDTH , CANVAS_HEIGHT)
-    enemiesArray.forEach(function(enemy){
-        enemy.update()
-        enemy.draw()
-    })
-    gameFrame++
-    requestAnimationFrame(animate)
-}
-animate()
